8fc8af236dbb9999a6393871602f92ff2e0344ed
samroodAli <devsamrood@gmail.com>
2021-03-26 08:43:33 +0530

    class promt user for guess and guess verification

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -6,7 +6,17 @@ class Mastermind
   end
 
   def print_matches(code)
-    puts "Exact matches:" + code.num_exact_matches.to_s
-    puts "Near matches:" + code.num_near_matches
+    puts @secret_code.num_exact_matches(code)
+    puts @secret_code.num_near_matches(code)
+  end
+
+  def ask_user_for_guess
+    puts "Enter a code"
+    input = gets.chomp
+    input = Code.from_string(input)
+    exact_matches = @secret_code.num_exact_matches(input)
+    puts exact_matches
+    puts @secret_code.num_near_matches(input)
+    @secret_code.length == exact_matches
   end
 end
